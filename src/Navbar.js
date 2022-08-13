0ddcb44906500d8ea37823a6c963f31ed98e6a4e
zeredogo <zeredogo@gmail.com>
2022-08-13 23:55:37 +0100

    updating commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,16 @@ import { useGlobalContext } from './context';
 
 
 const Navbar = () => {
-  const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext();
+  const {openSidebar, openSubmenu, closeSubmenu} = 
+  useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContext;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom })
+  }
+
 
   return <nav className='nav'>
     <div className='nav-center'>
@@ -17,13 +26,16 @@ const Navbar = () => {
       </div>
       <ul className='nav-links'>
         <li>
-          <button className='link-btn'>Products</button>
+          <button className='link-btn' onMouseOver=
+          {displaySubmenu}>Products</button>
         </li>
         <li>
-          <button className='link-btn'>developers</button>
+          <button className='link-btn' onMouseOver=
+          {displaySubmenu}>developers</button>
         </li>
         <li>
-          <button className='link-btn'>company</button>
+          <button className='link-btn' onMouseOver=
+          {displaySubmenu}>company</button>
         </li>
       </ul>
        <button className='btn signin-btn'>Sign in</button>
