9ea7bd1997fd56cf8a35e95fb7a6856fe12fcddc
zeredogo <zeredogo@gmail.com>
2022-08-13 21:48:47 +0100

    updating commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,6 +12,27 @@ const Sidebar = () => {
     <button className='close-btn' onClick={closeSidebar}>
       <FaTimes />
     </button>
+    <div className='sidebar-links'>
+      {sublinks.map((item, index) => {
+        const {links, page} = item
+        return (
+         <article key={index}>
+          <h4>{page}</h4>
+          <div className='sidebar-sublinks'>
+            {links.map((link, index) => {
+              const {url, icon, label} = link
+              return (
+                <a key={index} href={url}>
+                {icon}
+                {label}
+              </a>
+              )
+            })}
+          </div>
+         </article>
+        )
+      })}
+    </div>
   </div>
 </aside>
 }
