0ca3a02bcde91511d63df351557b5a86920b8dad
zeredogo <zeredogo@gmail.com>
2022-08-13 01:20:47 +0100

    making update

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,28 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalContext();
+  
+  return <section className='hero'>
+    <div className='hero-center'>
+      <article className='hero-info'>
+        <h1> Payments infrastructure for the internet </h1>
+        <p>
+          Millions of companies of all sizes-form startups to
+          Fortune 500s-use
+          stripe's software and APIs to accept payments, send 
+          payouts, and 
+          manage their businesses online.
+        </p>
+        <button className='btn'>
+          Start now
+        </button>
+      </article>
+      <article className='hero-images'>
+        <img src={phoneImg} className='phone-img' alt='phone' />
+      </article>
+    </div>
+  </section>
 }
 
-export default Hero
+export default Hero;
