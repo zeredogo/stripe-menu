556f1c22a723e69802ad6e8d2d880ef8eb2861ca
zeredogo <zeredogo@gmail.com>
2022-08-14 19:12:22 +0100

    updating commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,8 +15,13 @@ const Navbar = () => {
     openSubmenu(page, { center, bottom })
   }
 
+  const handleSubmenu = (e) => {
+    if(!e.target.classList.contains('link-btn')) {
+      closeSubmenu()
+    }
+  }
 
-  return <nav className='nav'>
+  return <nav className='nav' onMouseOver={handleSubmenu}>
     <div className='nav-center'>
       <div className='nav-header'>
         <img src={logo} className='nav-logo' alt='stripe' />
