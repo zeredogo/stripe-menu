de58925d568e32b77fc654a49b63b0986fe732e2
zeredogo <zeredogo@gmail.com>
2022-10-03 01:13:34 +0100

    update

--- src/Hero.js
+++ src/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
           stripe's software and APIs to accept payments, send 
           payouts, and 
           manage their businesses online. and this makes your 
-          expirence great wh
+          expirence great whil
         </p>
         <button className='btn'>
           Start now
